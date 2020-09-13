404de05c5d703f4962c059c7250e8f3332e9fc32
Adjéri SABI <kod047@gmail.com>
2020-09-14 07:14:07 +0900

    first commit, joke setup, listing  class

---
+++ android/app/src/main/kotlin/com/joke/adjeri/joke_app_orientation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joke.adjeri.joke_app_orientation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/joke_listing.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:joke_app_orientation/model/joke.dart';
+
+class JokeListing extends StatelessWidget {
+  final ValueChanged<Joke> jokeSelectedCallback;
+  final Joke jokeSelected;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: jokesList.map((joke){
+        return Container(
+          child: ListTile(
+            title: Text(joke.setup),
+            onTap: () => jokeSelectedCallback(joke),
+            selected: jokeSelected == joke,
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(4.5)
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  JokeListing({@required this.jokeSelectedCallback, this.jokeSelected});
+}
+
