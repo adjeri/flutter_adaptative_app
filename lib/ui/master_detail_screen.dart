a99d899b106f3176664545c608d90f50a30658be
Adjéri SABI <kod047@gmail.com>
2020-09-14 07:54:35 +0900

    final product

--- lib/ui/master_detail_screen.dart
+++ lib/ui/master_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:joke_app_orientation/model/joke.dart';
+import 'package:joke_app_orientation/ui/joke_details.dart';
+import 'package:joke_app_orientation/ui/joke_listing.dart';
 
 class MasterDetailScreen extends StatefulWidget {
   @override
@@ -6,8 +9,61 @@ class MasterDetailScreen extends StatefulWidget {
 }
 
 class _MasterDetailScreenState extends State<MasterDetailScreen> {
+  static const int tabletBreakPoint = 600;
+  Joke _selectedJoke;
+
+  Widget _buildMobileLayout() {
+    return JokeListing(jokeSelectedCallback: (jokeSelected) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (BuildContext context) {
+        return JokeDetails(
+          isInTabletLayout: false,
+          joke: jokeSelected,
+        );
+      }));
+    });
+  }
+
+  Widget _buildTabletLayout() {
+    return Row(
+      children: [
+        Flexible(
+            flex: 1,
+            child: Material(
+              elevation: 10,
+              child: JokeListing(
+                jokeSelectedCallback: (joke) {
+                  setState(() {
+                    _selectedJoke = joke;
+                  });
+                },
+                jokeSelected: _selectedJoke,
+              ),
+            )),
+        Flexible(
+          flex: 3,
+          child: JokeDetails(isInTabletLayout: true, joke: _selectedJoke),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget content;
+    var shortestSize = MediaQuery.of(context).size.shortestSide;
+    var orientation  = MediaQuery.of(context).orientation;
+    if(orientation == Orientation.portrait && shortestSize < tabletBreakPoint){
+      content = _buildMobileLayout();
+    }
+    else{
+      content = _buildTabletLayout();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jokes"),
+      ),
+      body: content,
+    );
   }
 }
